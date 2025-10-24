eee7e42e02245c18d546aef0f4a1a5556f39ad41
FadnisW <fadnisw@gmail.com>
2025-10-25 02:27:51 +0530

    Another Commit

--- src/app/(dashboard)/list/students/[id]/page.tsx
+++ src/app/(dashboard)/list/students/[id]/page.tsx
@@ -125,7 +125,7 @@ const SingleStudentPage = () => {
             <Link className="p-3 rounded-md bg-lamaSkyLight" href="/">
               Student&apos;s Lessons
             </Link>
-            <Link className="p-3 rounded-md bg-lamaPurpleLight" href="/">
+            <Link className="p-3 rounded-md bg-lamaPurpleLight" href={`/list/teachers?ClassId=${2}`}>
               Student&apos;s Teachers
             </Link>
             <Link className="p-3 rounded-md bg-pink-50" href="/">
